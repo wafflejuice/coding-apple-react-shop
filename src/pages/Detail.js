7bae9cf4776ab5bbeabb6c8b256de372b627c013
wafflejuice <wafflejuice96@gmail.com>
2023-05-01 09:40:15 +0900

    useEffect

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
+    let [discountBannerVisible, setDiscountBannerVisible] = useState(true);
+
+    let [count, setCount] = useState(0);
+    let [userInput, setUserInput] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => { setDiscountBannerVisible(false) }, 2000);
+    }, [])
+
+    useEffect(() => {
+        if (isNaN(userInput)) {
+            alert('not integer');
+        }
+    }, [userInput])
+
     let { id } = useParams();
     let shoe = props.shoesList.find((shoes) => shoes.id == id)
 
     return (
         <div className="container">
+            {
+                discountBannerVisible ?
+                    <div className="alert alert-warning">
+                        2초 이내 구매시 할인
+                    </div> : null
+            }
+
+            <input onChange={(e) => {
+                setUserInput(e.target.value)
+            }}></input>
+
+            <button onClick={() => { setCount(count + 1) }}>button</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src={`${process.env.PUBLIC_URL}/img/shoes${id}.jpg`} width='100%'></img>
